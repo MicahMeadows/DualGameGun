883ead7a5b0c339b9cda34b5969711ae31275631
Micah Meadows <micahm213@gmail.com>
2024-12-15 00:18:06 -0500

    Add espnow shoot message

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <Arduino.h>
 #include <IRremote.h>
+#include <WiFi.h>
+#include <esp_now.h>
+#include "messages/shoot_message.h"
+
+uint8_t towerMac[] = { 0x2C, 0xBC, 0xBB, 0x0D, 0xFB, 0x90 };
+
+esp_now_peer_info_t peerInfo;
+
+shoot_message shotMessage;
+
+void OnDataSent(const uint8_t *macAddress, esp_now_send_status_t status) {
+  Serial.print("\r\n Packet Send Status::\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 const int IR_LED_PIN = 23;
 const int RED_BTN_PIN = 25;
@@ -11,6 +25,25 @@ const uint8_t SHOOT_COMMAND_REPEATS = 3;
 
 void setup() {
   Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  memcpy(peerInfo.peer_addr, towerMac, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+
   pinMode(IR_LED_PIN, OUTPUT);
   pinMode(RED_BTN_PIN, INPUT_PULLUP);
   pinMode(BLUE_BTN_PIN, INPUT_PULLUP);
@@ -18,22 +51,34 @@ void setup() {
   IrSender.begin(IR_LED_PIN);
 }
 
-void shoot(uint8_t command)
+void shoot(bool isRed)
 {
-  IrSender.sendNEC(0x00, command, SHOOT_COMMAND_REPEATS);
+  shotMessage.type = 1;
+  shotMessage.isRed = isRed;
+  esp_err_t result = esp_now_send(towerMac, (uint8_t *) &shotMessage, sizeof(shotMessage));
+
+  if (result == ESP_OK) {
+    Serial.println("Shot message sent successfully!");
+  } else {
+    Serial.println("Failed to send shot message");
+  }
+
+  uint8_t shootCommand = isRed ? RED_SHOOT_COMMAND : BLUE_SHOOT_COMMAND;
+  IrSender.sendNEC(0x00, shootCommand, SHOOT_COMMAND_REPEATS);
+
 }
 
 void loop() {
   int redBtnState = digitalRead(RED_BTN_PIN);
   int blueBtnState = digitalRead(BLUE_BTN_PIN);
-  Serial.println("red: " + String(redBtnState));
-  Serial.println("blue: " + String(blueBtnState));
+  // Serial.println("red: " + String(redBtnState));
+  // Serial.println("blue: " + String(blueBtnState));
   if (redBtnState == LOW) {
     shoot(RED_SHOOT_COMMAND);
-    delay(1000);
+    delay(500);
   }
   else if (blueBtnState == LOW) {
     shoot(BLUE_SHOOT_COMMAND);
-    delay(1000);
+    delay(500);
   }
 }
