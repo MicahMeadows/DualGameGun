c227fd7ea9e540bf2c55018c202825427a22ba5d
Micah Meadows <micahm213@gmail.com>
2024-12-17 15:30:27 -0500

    Add hammer and safety btn

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include <IRremote.h>
 #include <WiFi.h>
 #include <esp_now.h>
-#include "messages/shoot_message.h"
+#include "messages.h"
 
 uint8_t towerMac[] = { 0x2C, 0xBC, 0xBB, 0x0D, 0xFB, 0x90 };
 
 esp_now_peer_info_t peerInfo;
 
 shoot_message shotMessage;
+
+btn_press_message btnMessage;
 
 void OnDataSent(const uint8_t *macAddress, esp_now_send_status_t status) {
   Serial.print("\r\n Packet Send Status::\t");
@@ -17,10 +19,13 @@ void OnDataSent(const uint8_t *macAddress, esp_now_send_status_t status) {
 
 const int IR_LED_PIN = 23;
 const int RED_BTN_PIN = 25;
-const int BLUE_BTN_PIN = 33;
+const int TRIGGER_BTN_PIN = 33;
+const int HAMMER_BTN_PIN = 32;
+const int SAFETY_BTN_PIN = 25;
 
-const uint8_t RED_SHOOT_COMMAND = 0x35;
-const uint8_t BLUE_SHOOT_COMMAND = 0x7;
+// const uint8_t RED_SHOOT_COMMAND = 0x35;
+// const uint8_t BLUE_SHOOT_COMMAND = 0x7;
+const uint8_t SHOOT_COMMAND = 0x7;
 const uint8_t SHOOT_COMMAND_REPEATS = 3; 
 
 void setup() {
@@ -45,40 +50,71 @@ void setup() {
   }
 
   pinMode(IR_LED_PIN, OUTPUT);
-  pinMode(RED_BTN_PIN, INPUT_PULLUP);
-  pinMode(BLUE_BTN_PIN, INPUT_PULLUP);
+  // pinMode(RED_BTN_PIN, INPUT_PULLUP);
+  // pinMode(BLUE_BTN_PIN, INPUT_PULLUP);
+  pinMode(TRIGGER_BTN_PIN, INPUT_PULLUP);
+  pinMode(HAMMER_BTN_PIN, INPUT_PULLUP);
+  pinMode(SAFETY_BTN_PIN, INPUT_PULLUP);
 
   IrSender.begin(IR_LED_PIN);
 }
 
-void shoot(bool isRed)
-{
-  shotMessage.type = 1;
-  shotMessage.isRed = isRed;
-  esp_err_t result = esp_now_send(towerMac, (uint8_t *) &shotMessage, sizeof(shotMessage));
-
+void sendButtonMessage(uint8_t button, bool value) {
+  btnMessage.type = 2;
+  btnMessage.button = button;
+  btnMessage.value = value;
+  
+  esp_err_t result = esp_now_send(towerMac, (uint8_t *) &btnMessage, sizeof(btnMessage));
   if (result == ESP_OK) {
-    Serial.println("Shot message sent successfully!");
+    Serial.println("Btn [" + (String)button + "] message sent successfully!");
   } else {
-    Serial.println("Failed to send shot message");
+    Serial.println("Failed to send btn trigger message");
   }
+}
 
-  uint8_t shootCommand = isRed ? RED_SHOOT_COMMAND : BLUE_SHOOT_COMMAND;
+// void shoot(bool isRed)
+void handleTriggerClick()
+{
+  sendButtonMessage(0, true);
+
+  uint8_t shootCommand = SHOOT_COMMAND;
   IrSender.sendNEC(0x00, shootCommand, SHOOT_COMMAND_REPEATS);
+}
 
+void handleSafetyClick() {
+  sendButtonMessage(2, true);
+}
+
+void handleHammerClick() {
+  sendButtonMessage(1, true);
 }
 
 void loop() {
-  int redBtnState = digitalRead(RED_BTN_PIN);
-  int blueBtnState = digitalRead(BLUE_BTN_PIN);
+  // int redBtnState = digitalRead(RED_BTN_PIN);
+  // int blueBtnState = digitalRead(BLUE_BTN_PIN);
+  int triggerBtnState = digitalRead(TRIGGER_BTN_PIN);
+  int hammerBtnState = digitalRead(HAMMER_BTN_PIN);
+  int safetyBtnState = digitalRead(SAFETY_BTN_PIN);
   // Serial.println("red: " + String(redBtnState));
   // Serial.println("blue: " + String(blueBtnState));
-  if (redBtnState == LOW) {
-    shoot(RED_SHOOT_COMMAND);
+  if (triggerBtnState == LOW) {
+    handleTriggerClick();
     delay(500);
   }
-  else if (blueBtnState == LOW) {
-    shoot(BLUE_SHOOT_COMMAND);
+  if (safetyBtnState == LOW) {
+    handleSafetyClick();
     delay(500);
   }
+  if (hammerBtnState == LOW) {
+    handleHammerClick();
+    delay(500);
+  }
+  // if (redBtnState == LOW) {
+  //   shoot(RED_SHOOT_COMMAND);
+  //   delay(500);
+  // }
+  // else if (blueBtnState == LOW) {
+  //   shoot(BLUE_SHOOT_COMMAND);
+  //   delay(500);
+  // }
 }
